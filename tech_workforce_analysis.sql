b55a3227a50eb4075056a55a1453ae36a57c327a
Marta N. <141928743+marta1895@users.noreply.github.com>
2026-03-11 10:39:57 -0400

    Update tech_workforce_analysis.sql

--- tech_workforce_analysis.sql
+++ tech_workforce_analysis.sql
@@ -8,6 +8,7 @@
 -- Companies are divided into three main groups based on their business model:
 -- FAANG, Established Tech, and Newer / Gig Economy.
 -- This makes the further analysis clearer and easier to compare.
+
 WITH company_totals AS (
     SELECT 
         CASE
@@ -20,6 +21,7 @@ WITH company_totals AS (
     FROM tech_wf
     GROUP BY 1, 2
 )
+	
 SELECT
     company_group,
     ROUND(SUM(revenue) / 1000, 2) AS total_revenue_trillions_usd --converted the revenue outputs from billions to trillions for better readability
@@ -29,6 +31,7 @@ GROUP BY 1
 ORDER BY 2 DESC;
 
 -- 2. As the AI Expansion changed market dynamics, I analyzed total revenue per company group before and after the AI Expansion period
+
 WITH company_totals AS (
     SELECT 
         CASE
@@ -45,6 +48,7 @@ WITH company_totals AS (
     FROM tech_wf
     GROUP BY 1, 2, 3
 )
+
 SELECT
     company_group,
     years,
@@ -52,10 +56,11 @@ SELECT
 FROM company_totals
 WHERE years IS NOT NULL
 GROUP BY 1, 2
-ORDER BY 1; -- Conclusion: The comparison shows that total revenue more than doubled in the period after the AI Expansion. 
+ORDER BY 1; -- Conclusion: The comparison shows that total revenue more than doubled in the period after the AI Expansion
 
 
 --3. Year-over-year net employee change by company group (2001–2025) 
+
 WITH company_totals AS (
     SELECT 
         CASE
@@ -68,15 +73,18 @@ WITH company_totals AS (
     FROM tech_wf
     GROUP BY 1, 2
 )
+	
 SELECT 
     company_group,
     year,
     total_net_change
 FROM company_totals
-ORDER BY 1, 2; -- Conclusion: Tech workforce growth followed a synchronized hyper-expansion during the pandemic, led by FAANG, followed by a sharp industry-wide correction beginning in 2022
+ORDER BY 1, 2; -- Conclusion: Tech workforce growth followed a synchronized hyper-expansion during the pandemic, 
+               -- led by FAANG, followed by a sharp industry-wide correction beginning in 2022
 
 
--- 4. Changes in hiring rate, attrition rate and GDP growth (US) based on company group during major economical periods
+-- 4. Changes in hiring rate, attrition rate and GDP growth (US) based on company group during major economic periods
+
 WITH company_group_creation AS (
     SELECT 
         CASE
@@ -112,11 +120,11 @@ SELECT
     company_group,
     economic_period,
     ROUND(AVG(hiring_rate_pct), 2) AS avg_hiring_rate_pct,
-    ROUND(AVG(attrition_rate_pct), 2) AS avg_layoffs_rate_pct,
+    ROUND(AVG(attrition_rate_pct), 2) AS avg_attrition_rate_pct,
     ROUND(AVG(gdp_growth_us_pct), 2) AS avg_gdp_growth_us_pct
 FROM economic_periods_creation
 WHERE economic_period IS NOT NULL
-GROUP BY company_group, economic_period
+GROUP BY 1, 2
 ORDER BY
     CASE economic_period
         WHEN 'The Dot-Com Correction' THEN 1
@@ -125,10 +133,12 @@ ORDER BY
         WHEN 'Pandemic Hyper-Growth' THEN 4
         WHEN 'The AI Expansion' THEN 5
     END,
-    company_group; -- Conclusion: Hiring peaked during the early digital expansion cycles, while the pandemic period further accelerated workforce growth across all groups. 
+    1; -- Conclusion: Hiring peaked during the early digital expansion cycles, while the pandemic period further accelerated workforce growth across all groups; 
                    -- The AI expansion represents a structural shift, with tech sector growth now driven more by productivity than by headcount
 
--- 5. Changes in total comapny revenue and weighted avg revenue per employee based on company group during major economical periods
+
+-- 5. Changes in total company revenue and weighted average revenue per employee based on the company group during major economic periods
+
 WITH company_group_creation AS (
     SELECT 
         CASE
@@ -166,7 +176,7 @@ SELECT
         AS weighted_avg_revenue_per_employee
 FROM economic_periods_creation
 WHERE economic_period IS NOT NULL
-GROUP BY company_group, economic_period
+GROUP BY 1, 2
 ORDER BY
     CASE economic_period
         WHEN 'The Dot-Com Correction' THEN 1
@@ -175,11 +185,13 @@ ORDER BY
         WHEN 'Pandemic Hyper-Growth' THEN 4
         WHEN 'The AI Expansion' THEN 5
     END,
-    company_group; -- Conclusion: Tech companies are generating record revenue per employee during the AI Expansion period, signaling a shift from workforce-driven growth to productivity-driven growth.
+    1; -- Conclusion: Tech companies are generating record revenue per employee during the AI Expansion period, 
+                   -- signaling a shift from workforce-driven growth to productivity-driven growth
 
 
 -- 6. How does the market react to workforce changes?
---Finding how does the workforce changes affect company revenue, revenue per employee and stock price per company group
+-- Finding how the changes in the workforce affect company revenue, revenue per employee and stock price per company group
+
 WITH company_groups AS (
     SELECT 
         company,
@@ -196,6 +208,7 @@ WITH company_groups AS (
         stock_price_change_pct
     FROM tech_wf
 ),
+	
 next_year_metrics AS (
     SELECT
         t1.company,
@@ -211,6 +224,7 @@ next_year_metrics AS (
         ON t1.company = t2.company
         AND t2.year = t1.year + 1
 ),
+	
 stock_categorized AS (
     SELECT
         company,
@@ -237,13 +251,13 @@ SELECT
     stock_performance_category,
     company_group,
     COUNT(*) AS case_count,
-    ROUND(AVG(attrition_rate_pct), 2) AS avg_attrition,
+    ROUND(AVG(attrition_rate_pct), 2) AS avg_attrition_rate,
     ROUND(AVG(hiring_rate_pct), 2) AS avg_hiring_rate,
     ROUND(AVG(next_year_revenue_change), 2) AS avg_revenue_change,
     ROUND(AVG(next_year_revenue_per_employee), 0) AS avg_rev_per_employee,
     ROUND(AVG(next_year_stock_change), 1) AS avg_stock_change
 FROM stock_categorized
-GROUP BY stock_performance_category, company_group
+GROUP BY 1, 2
 ORDER BY 
     CASE stock_performance_category
         WHEN 'Explosive Growth' THEN 1
@@ -255,13 +269,16 @@ ORDER BY
         WHEN 'Severe Drawdown' THEN 7
         WHEN 'Crash' THEN 8
     END,
-    company_group -- Conclusion: Attrition doesn't matter, it's flat across every category. Hiring rate is actually a warning sign, not a strength; 
+    2 -- Conclusion: Attrition doesn't matter, it's flat across every category. Hiring rate is actually a warning sign, not a strength; 
                   -- Newer/Gig Economy companies hired most aggressively right before their worst stock years. 
-                  -- Revenue per employee is the real predictor: higher efficiency consistently correlates with better stock outcomes, with FAANG leading at $1M–$1.9M per employee vs Established Tech at $450K–$850K
+                  -- Revenue per employee is the real predictor: higher efficiency consistently correlates with better stock outcomes, 
+	              -- with FAANG leading at $1M–$1.9M per employee vs Established Tech at $450K–$850K
 
     
 -- 7. How the Workforce Volatility was changing in different Tech Groups Across Economic Cycles
---Note: Volatility metrics for the Newer/Gig Economy group prior to 2005 are unavailable/zero due to the limited number of companies established in that sector at the time
+-- Note: Volatility metrics for the Newer/Gig Economy group prior to 2005 are unavailable/zero
+-- due to the limited number of companies established in that sector at the time
+	
 WITH company_group_creation AS (
     SELECT 
         CASE
@@ -296,11 +313,11 @@ SELECT
     economic_period,
     ROUND(AVG(hiring_rate_pct), 2) AS avg_hiring_rate_pct,
     ROUND(COALESCE(STDDEV(hiring_rate_pct), 0), 2) AS hiring_volatility,
-    ROUND(AVG(attrition_rate_pct), 2) AS avg_layoffs_rate_pct,
+    ROUND(AVG(attrition_rate_pct), 2) AS avg_attrition_rate_pct,
     ROUND(COALESCE(STDDEV(attrition_rate_pct), 0), 2) AS attrition_volatility
 FROM economic_periods_creation
 WHERE economic_period IS NOT NULL
-GROUP BY company_group, economic_period
+GROUP BY 1, 2
 ORDER BY
     CASE economic_period
         WHEN 'The Dot-Com Correction' THEN 1
@@ -309,14 +326,15 @@ ORDER BY
         WHEN 'Pandemic Hyper-Growth' THEN 4
         WHEN 'The AI Expansion' THEN 5
     END,
-    company_group; -- Conclusion: Newer companies chase growth, but established giants provide stability. 
+    1; -- Conclusion: Newer companies chase growth, but established giants provide stability. 
                    -- Since 2023, the entire industry has shifted its focus: it’s no longer about how many people you hire, but how much revenue each person generates
  
     
     
- -- Additional drill-down on Question 6: Investigating the FAANG outlier from the scatter plot
+-- Additional drill-down on Question 6: Investigating the FAANG outlier from the scatter plot
 -- FAANG showed unusually high revenue per employee (~$1.85M) during severe stock downturns
 -- This query identifies which companies and years drove that result 
+
 WITH b1 AS (
     SELECT
         t1.company as company_t1,
@@ -339,7 +357,7 @@ WITH b1 AS (
     JOIN tech_wf t2
         ON t1.company = t2.company
         AND t2.year = t1.year + 1 
-	ORDER BY next_year_stock_change ASC, next_year_revenue_per_employee DESC
+	ORDER BY 6 ASC, 5 DESC
  ),
  
  b2 AS (
@@ -374,5 +392,5 @@ SELECT
 FROM b2
 WHERE company_group = 'FAANG'
 	AND
-	stock_performance_category = 'Severe Drawdown'
+	stock_performance_category = 'Severe Drawdown';
 
